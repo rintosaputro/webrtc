1590c196ed1064edfdfe3f3f9ef59f9cfd40c4d0
Rinto Saputro <rinto.saputro@digitalsekuriti.id>
2024-12-20 14:02:54 +0700

    feat: show call elements related with call type

--- public/js/ui.js
+++ public/js/ui.js
@@ -75,3 +75,73 @@ export const removeAllDialogs = () => {
   const dialog = document.getElementById("dialog");
   dialog.querySelectorAll("*").forEach((n) => n.remove());
 };
+
+export const showCallElements = (callType) => {
+  if (callType === constants.callType.CHAT_PERSONAL_CODE) {
+    showChatCallElements();
+  }
+
+  if (callType === constants.callType.VIDEO_PERSONAL_CODE) {
+    showVideoCallElements();
+  }
+};
+
+const showChatCallElements = () => {
+  const finishConnectionChatButtonContainer = document.getElementById(
+    "finish_chat_button_container"
+  );
+  showElement(finishConnectionChatButtonContainer);
+
+  const newMessageInput = document.getElementById("new_message");
+  showElement(newMessageInput);
+
+  // block panel
+  disableDashboard();
+};
+
+const showVideoCallElements = () => {
+  const callButtons = document.getElementById("call_buttons");
+  showElement(callButtons);
+
+  const placeholder = document.getElementById("video_placeholder");
+  hideElement(placeholder);
+
+  const remoteVideo = document.getElementById("remote_video");
+  showElement(remoteVideo);
+
+  const newMessageInput = document.getElementById("new_message");
+  showElement(newMessageInput);
+
+  // block panel
+  disableDashboard();
+};
+
+// ui helper functions
+
+const enableDashboard = () => {
+  const dahsboardBlocker = document.getElementById("dashboard_blur");
+
+  if (!dahsboardBlocker.classList.contains("display_none")) {
+    dahsboardBlocker.classList.add("display_none");
+  }
+};
+
+const disableDashboard = () => {
+  const dahsboardBlocker = document.getElementById("dashboard_blur");
+
+  if (dahsboardBlocker.classList.contains("display_none")) {
+    dahsboardBlocker.classList.remove("display_none");
+  }
+};
+
+const hideElement = (element) => {
+  if (!element.classList.contains("display_none")) {
+    element.classList.add("display_none");
+  }
+};
+
+const showElement = (element) => {
+  if (element.classList.contains("display_none")) {
+    element.classList.remove("display_none");
+  }
+};
