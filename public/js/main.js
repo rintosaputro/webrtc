365262235f0718c3878135f60ef3a43377b82240
Rinto Saputro <rinto.saputro@digitalsekuriti.id>
2024-12-24 09:50:41 +0700

    feat: add functionallity turn on/off  mic and video

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ import * as store from "./store.js";
 import * as wss from "./wss.js";
 import * as webRTCHandler from "./webRTCHandler.js";
 import * as constants from "./constants.js";
-import { getIncomingCallDialog } from "./elements.js";
+import * as ui from "./ui.js";
 
 // initialize socket.io connection
 
@@ -49,8 +49,20 @@ personalCodeVideoButton.addEventListener("click", () => {
   webRTCHandler.sendPreOffer(callType, callePersonalCode);
 });
 
-getIncomingCallDialog(
-  "VIDEO",
-  () => {},
-  () => {}
-);
+const micButton = document.getElementById("mic_button");
+micButton.addEventListener("click", () => {
+  const localStream = store.getState().localStream;
+  const micEnabled = localStream.getAudioTracks()[0].enabled;
+  localStream.getAudioTracks()[0].enabled = !micEnabled;
+
+  ui.updateMicButton(micEnabled);
+});
+
+const cameraButton = document.getElementById("camera_button");
+cameraButton.addEventListener("click", () => {
+  const localStream = store.getState().localStream;
+  const cameraEnabled = localStream.getVideoTracks()[0].enabled;
+  localStream.getVideoTracks()[0].enabled = !cameraEnabled;
+
+  ui.updateCameraButton(cameraEnabled);
+});
